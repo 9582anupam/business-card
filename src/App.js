c03840204432253d4b707cb370866a62cda8af04
Anupam <9582anupamk@gmail.com>
2024-08-15 12:30:07 +0530

    breakpoints

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Cards from "./components/Cards";
 
 function App() {
     return (
         <div className="App text-center font-bold underline">
             Hello world
+            <Cards />
         </div>
     );
 }
